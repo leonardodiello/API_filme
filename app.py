14aaa17a0f607de98f2a4d99f35dae671419bd55
Leonardo Diello <leonardodiello4@gmail.com>
2026-09-22 20:09:50 -0400

    estrutura do projeto pronta

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-##import requests
+import requests
 
 from dotenv import load_dotenv
 
@@ -7,4 +7,16 @@ load_dotenv()
 
 api=os.getenv("api_key")
 
-print(api)
+titulo = input("Digite o nome do filme que você quer saber mais: ")
+
+url = "http://www.omdbapi.com/"
+params = {
+    "apikey": api,
+    "t": titulo
+}
+
+resposta = requests.get(url, params=params)
+
+dados = resposta.json()
+
+print(dados)
